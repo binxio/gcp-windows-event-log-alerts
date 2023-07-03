5d3d464892ea3adfcee5dbce2e5c8e40dbc81faa
Laurens Knoll <3205006+laurensknoll@users.noreply.github.com>
2023-07-03 13:45:54 +0200

    Added FailureTask not running-alert example

--- README.md
+++ README.md
@@ -23,8 +23,9 @@ Tired of checking your service statuses every day? Use the Windows event logs to
 
 2. Configure alert policies to send out notifications
 
-    Add an alerting policy for your scheduled task.
+    Add an alerting policy for scheduled task failures and inactive tasks.
 
+    Task failure alert
     ```
     resource.type="gce_instance"
 
@@ -37,9 +38,28 @@ Tired of checking your service statuses every day? Use the Windows event logs to
     jsonPayload.Message !~ "return code 0\.$"
     ```
 
+    Inactive task logs metric and alert
+    ```
+    -- Filter action completed logs for scheduled task
+    resource.type="gce_instance"
+    jsonPayload.Channel="Microsoft-Windows-TaskScheduler/Operational"
+    jsonPayload.EventID="201"
+    jsonPayload.StringInserts="\\FailureTask"
+    
+    -- Alert on absent metrics
+    condition_absent {
+        ..
+        filter = 'resource.type = "gce_instance" AND metric.type = "logging.googleapis.com/user/${google_logging_metric.failuretask_not_running.name}"'
+        duration = "1800s"
+        ..
+    }
+    ```
+
 
 ## Deployment
 
 First, update the [Terraform variables](terraform/variables.tf) to deploy to your project and alert to your email address.
 
-Second, run `terraform apply` to deploy an example VM that runs a failing scheduled task. It takes about 10 minutes before you receive the alert.
+Second, run `terraform apply` to deploy an example VM that runs a failing scheduled task. It takes about 10 minutes before you receive the *Task failed*-alert.
+
+Third, stop the VM to receive the *Task not running*-alert. This takes about 15 minutes.
--- terraform/task_alert.tf
+++ terraform/task_alert.tf
@@ -49,3 +49,79 @@ resource "google_monitoring_alert_policy" "failuretask_failed" {
     auto_close = "1800s"
   }
 }
+
+## FailureTask alert; sends an email on FailureTask not running.
+resource "google_logging_metric" "failuretask_not_running" {
+  project = var.project_id
+  name   = "task-runner/failuretask_run_count"
+
+  filter = <<EOT
+  resource.type="gce_instance"
+  jsonPayload.Channel="Microsoft-Windows-TaskScheduler/Operational"
+  jsonPayload.EventID="201"
+  jsonPayload.StringInserts="\\FailureTask"
+  EOT
+
+  metric_descriptor {
+    metric_kind = "DELTA"
+    value_type  = "INT64"
+
+    labels {
+      key = "host_name"
+      value_type = "STRING"
+    }
+  }
+
+  label_extractors = {
+    "host_name" = "EXTRACT(jsonPayload.Computer)"
+  }
+}
+
+resource "google_monitoring_alert_policy" "failuretask_not_running" {
+  project = var.project_id
+
+  display_name = "FailureTask not running"
+
+  notification_channels = [
+    google_monitoring_notification_channel.scheduled_task_status_email.id,
+  ]
+
+  documentation {
+    mime_type = "text/markdown"
+    content   = <<EOT
+    FailureTask not running!
+
+    Ensure that the $${metadata.system_label.name} VM instance runs and the scheduled task is enabled ...
+  
+    [Quick link to the VM](https://console.cloud.google.com/compute/instancesDetail/zones/$${resource.label.zone}/instances/$${resource.label.instance_id}?project=$${resource.label.project_id})
+    EOT
+  }
+
+  combiner = "OR"
+  conditions {
+    display_name = "FailureTask not running"
+
+    condition_absent {
+      filter = <<EOT
+      resource.type = "gce_instance" AND metric.type = "logging.googleapis.com/user/${google_logging_metric.failuretask_not_running.name}"
+      EOT
+      aggregations {
+        group_by_fields = [
+          "metadata.system_labels.name",
+        ]
+        alignment_period = "300s"
+        per_series_aligner = "ALIGN_SUM"
+        cross_series_reducer = "REDUCE_COUNT"
+      }
+      
+      duration = "600s" # Fail after 10 minutes without events
+      trigger {
+        count = 1
+      }
+    }
+  }
+
+  alert_strategy {
+    auto_close = "3600s"
+  }
+}
